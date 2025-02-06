3367b8aeb4cbdf0da4a1dc8290a1792175abea3e
Siuzanna Abkadyrova <siuzanna.abkadyrova@softwire.com>
2025-02-06 13:44:52 +0000

    Added button for changing posts background colour

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
 });
+
+document.getElementById('post-list-colour-change-button').onclick = () => {
+  const posts = document.getElementsByClassName('post-container');
+
+  for (const post of posts) {
+    post.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+  }
+};
+
